b06fe09df37d6d5d0702d1840e95e9a00f0c4bcc
Kong Chae Won <chaewonkong@gmail.com>
2019-11-01 13:38:37 +0900

    build and deploy

---
+++ build/precache-manifest.b2cac495e25b828a38be55f61a73f62e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20c684d983cc27d70bc0355bd96bc5e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc2a697d42d3767ed90e",
+    "url": "/static/css/2.41a373cd.chunk.css"
+  },
+  {
+    "revision": "dc2a697d42d3767ed90e",
+    "url": "/static/js/2.8588c14c.chunk.js"
+  },
+  {
+    "revision": "1afb47ab8371885fa434",
+    "url": "/static/js/main.3d3f86d7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
